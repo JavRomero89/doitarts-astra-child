10440c9d9bfebd3ed5a030b760d795bbb6803e4e
JavRomero89 <javieromero23@gmail.com>
2026-01-13 02:02:16 +0100

    Deploy React build to /dist and align WordPress enqueue

--- functions.php
+++ functions.php
@@ -2,14 +2,15 @@
 if (!defined('ABSPATH')) exit;
 
 add_action('wp_enqueue_scripts', function () {
-    $bundle_path = get_stylesheet_directory() . '/assets/dist/bundle.js';
-    if (!file_exists($bundle_path)) return;
+    $bundle = get_stylesheet_directory() . '/dist/bundle.js';
+    if (!file_exists($bundle)) return;
 
     wp_enqueue_script(
-        'doitarts-react-bundle',
-        get_stylesheet_directory_uri() . '/assets/dist/bundle.js',
+        'doitarts-react',
+        get_stylesheet_directory_uri() . '/dist/bundle.js',
         [],
-        filemtime($bundle_path),
+        filemtime($bundle),
         true
     );
-}, 30);
+});
+
