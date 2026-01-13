cd1589460989701981a6976ddad30dc35d6cafab
JavRomero89 <javieromero23@gmail.com>
2026-01-13 02:57:05 +0100

    React test visible

--- assets/src/ProductCustomizer.jsx
+++ assets/src/ProductCustomizer.jsx
@@ -1,6 +1,6 @@
 export default function ProductCustomizer() {
   return (
-    <div style={{ padding: 20, border: '2px solid red' }}>
+    <div style={{ padding: 20, border: '3px solid red', fontSize: 20 }}>
       REACT FUNCIONA ✅
     </div>
   )
